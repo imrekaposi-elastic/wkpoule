e4b74616dc19d997b925506f7ed2ee3e474d254a
Imre Kaposi <imre@kaposi.net>
2026-05-09 21:46:03 +0200

    bumped version number after locking modification

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,7 +44,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     title="Worldcup 2026 game",
-    version="1.5.0",
+    version="1.5.1",
     description="World Cup 2026 prediction game",
     lifespan=lifespan,
 )
